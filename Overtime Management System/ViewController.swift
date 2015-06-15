c21d91de23bf94ab3fa1b123b956ba44ae8a05d9
panzhenhai <panzhenhai@panzhenhaimatoMacBook-Pro.local>
2015-06-15 11:55:47 +0900

    Initial Commit

---
+++ Overtime Management System/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Overtime Management System
+//
+//  Created by panzhenhai on 15/6/15.
+//  Copyright (c) 2015年 panzhenhai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Overtime Management SystemTests/Overtime_Management_SystemTests.swift
@@ -0,0 +1,36 @@
+//
+//  Overtime_Management_SystemTests.swift
+//  Overtime Management SystemTests
+//
+//  Created by panzhenhai on 15/6/15.
+//  Copyright (c) 2015年 panzhenhai. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Overtime_Management_SystemTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
